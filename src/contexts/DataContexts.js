658d3bb2c86300e5eb254053d7751550464cc152
gorkemgokcek <gorkemgokcekk@gmail.com>
2024-02-06 13:19:37 +0300

    fakedata and datacontext added

---
+++ src/contexts/DataContexts.js
@@ -0,0 +1,13 @@
+import {createContext, useState} from "react";
+import { fakeData } from "../data/fakeData";
+
+
+export const DataContext=createContext();
+
+export const DataProvider=({children})=>{
+    const [data, setData]=useState(fakeData);
+
+    return <DataContext.Provider value={{data, setData}}>
+        {children}
+    </DataContext.Provider>
+}
---
+++ src/data/fakeData.js
@@ -0,0 +1,74 @@
+export const fakeData = {
+    name: "Görkem Zeki Gökcek",
+    city: "Ankara",
+    birthDate: "10.01.1999",
+    school: "HacettepeUni",
+    rol: "Frontend",
+    headerImg:"images/hero-right12.jpg",
+    profile:{
+      img:"images/image2.jpg",
+      detail1:"Loremipsum, dolor sit  consectetur adipisicing elit. Veniamaut, odit laborum aliquam voluptatum nisi mollitia.",
+      detail2:"Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!"
+    },
+   
+    language: {
+      tr: {
+        translation: {
+          changeLang: "ENGLISH",
+          span: " to pass",
+          headerTitle: "Ben bir ön yüz geliştiricisiyim...",
+          headerDetail:
+            "...mükemmel kullanıcı deneyimlerine sahip, sağlam ve ölçeklenebilir ön uç ürünleri oluşturmayı seven.",
+          skills: "Yetenekler",
+          profile: "",
+          basicInfo: "Temel Bilgiler",
+          dateOfBirth: "Doğum Tarihi",
+          cityOfResidence: "İkamet Şehri",
+          educationalBackground: "Eğitim  Durumu",
+          preferredRole: "Tercih Ettiği Rol",
+          about: "Benim Hakkımda",
+          projetcs: "Projeler",
+          message: "Bana bir mesaj gönder",
+          footerDetail:
+            "Bir sorunuz veya öneriniz mi var, yoksa sadece merhaba mı demek istiyorsunuz? Devam et.",
+        },
+      },
+      en: {
+        translation: {
+          changeLang: "TÜRKÇE",
+          span: "'YE GEÇ",
+          headerTitle: "I am a Frontend Developer...",
+          headerDetail:
+            "...who likes to craft solid and scalable frontend products with great user experiences.",
+          skills: "Skills",
+          profile: "",
+          basicInfor: "Basic Information",
+          dateOfBirth: "Date of Birth",
+          cityOfResidence: " City of Residence",
+          educationalBackground: "Educational Background",
+          preferredRole: "Preferred Role",
+          about: "About Me",
+          projetcs: "Projects",
+          message: "Send me a message!",
+          footerDetail:
+            "Got a question or proposal, or just want to say hello? Go ahead.",
+        },
+      },
+    },
+    projetcs: [
+      {
+        img:"images/Rectangle41.jpg",
+        title:"Workintech",
+        detail:
+          "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+      },
+      {
+        img: "images/Rectangle31.jpg",
+        title: "Journey",
+        detail:
+          "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+      },
+    ],
+    
+  };
+  
