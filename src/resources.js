d2b485a4c01f1a617ba2f0c13527a7fd90622b45
gorkemgokcek <gorkemgokcekk@gmail.com>
2024-02-06 13:01:39 +0300

    resources add

---
+++ src/resources.js
@@ -0,0 +1,8 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+
+import { fakeData } from "./data/fakeData";
+
+export const resources= {
+    tr: fakeData.language.tr,
+    en: fakeData.language.en
+}
